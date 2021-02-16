ae7b5dcee66e04d38474ac0af6b8e8653de1b273
xcickai1 <xcickai1@stuba.sk>
2021-02-16 20:28:11 +0100

    Ostatnvo

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,5 +3,21 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        int celecislo =5;
+        long celeCisloVecie =5L;
+        char znak = 'a';
+        String retazec = "a";
+        float desatineSinglePrecision = 5.5f;
+        double desatinedoubleprecision =5.5;
+        int podiel =2/3; //0
+        System.out.println(podiel);
+        int zvisok = 2%3; //2
+        System.out.println(zvisok);
+        float podielDesatinny = 2/3; // vypise 0 , lebo robim podiel z celima
+        float podielDesatinny1 = 2/3.0f;
+        // boolean pravda = ture;
+
+
     }
 }
